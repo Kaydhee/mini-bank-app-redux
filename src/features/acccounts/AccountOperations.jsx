bdc17803d07de332d0909a965c87fb72f57efcfa
Daniel David <dannieimeh@gmail.com>
2023-12-30 22:42:21 +0100

    made use of redux toolkit to update and properly arrange project

--- src/features/acccounts/AccountOperations.jsx
+++ src/features/acccounts/AccountOperations.jsx
@@ -14,6 +14,7 @@ function AccountOperations() {
 		loan: currentLoan,
 		loanPurpose: currentLoanPurpose,
 		balance,
+		isLoading,
 	} = useSelector((store) => store.account);
 
 	console.log(balance);
@@ -21,8 +22,9 @@ function AccountOperations() {
 	function handleDeposit() {
 		if (!depositAmount) return;
 
-		dispatch(deposit(depositAmount));
+		dispatch(deposit(depositAmount, currency));
 		setDepositAmount('');
+		setCurrency('USD');
 	}
 
 	function handleWithdrawal() {
@@ -63,7 +65,11 @@ function AccountOperations() {
 						<option value='GBP'>British Pound</option>
 					</select>
 
-					<button onClick={handleDeposit}>Deposit {depositAmount}</button>
+					<button
+						onClick={handleDeposit}
+						disabled={isLoading}>
+						{isLoading ? 'converting...' : `Deposit ${depositAmount}`}
+					</button>
 				</div>
 
 				<div>
--- src/store.jsx
+++ src/store.jsx
@@ -1,31 +1,13 @@
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import accountReducer from './features/acccounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
 
-const rootReducer = combineReducers({
-	account: accountReducer,
-	customer: customerReducer,
+const store = configureStore({
+	reducer: {
+		account: accountReducer,
+		customer: customerReducer,
+	},
 });
 
-const store = createStore(rootReducer);
-
 export default store;
-
-// Normal reducer method
-// store.dispatch({ type: 'account/deposit', payload: 500 });
-// store.dispatch({ type: 'account/withdraw', payload: 200 });
-// store.dispatch({
-// 	type: 'account/requestLoan',
-// 	payload: { amount: 1000, purpose: 'to buy loud' },
-// });
-
-// store.dispatch({ type: 'account/payLoan' });
-
-// store.dispatch(deposit(500));
-// store.dispatch(withdraw(200));
-// store.dispatch(requestLoan(1000, 'to buy a car'));
-// store.dispatch(payLoan());
-
-// store.dispatch(createCustomer('Daniel David', '123456789'));
-// store.dispatch(updateName('Daniel Imeh David'));
